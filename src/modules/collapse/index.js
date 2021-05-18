fe398fedcae52ec6905b17362a098254983230a9
Carl Avidano <cavidano@gmail.com>
2021-05-18 18:25:43 -0400

    updating breadcrumb, etc

--- src/modules/collapse/index.js
+++ src/modules/collapse/index.js
@@ -11,16 +11,27 @@ export default class Collapse {
         const collapseButtonList = document.querySelectorAll("[data-toggle='collapse']");
 
         collapseButtonList.forEach((collapseButton) => {
-
+            
             collapseButton.setAttribute("aria-expanded", false);
-    
+            
             collapseButton.addEventListener("click", (event) => {
+
+                const currentCollapse = event.target;
     
-                const collapseTargetID = event.target.getAttribute("data-target").replace(/#/, "");
+                const collapseTargetID = currentCollapse.getAttribute("data-target").replace(/#/, "");
     
                 const collapseTarget = document.getElementById(collapseTargetID);
-    
+
+                let expanded = currentCollapse.getAttribute("aria-expanded");
+
+                if (expanded === "true") {
+                    collapseButton.setAttribute("aria-expanded", false);
+                } else if (expanded === "false") {
+                    collapseButton.setAttribute("aria-expanded", true);
+                }
+                
                 collapseTarget.classList.toggle("shown");
+    
             });
 
         });
