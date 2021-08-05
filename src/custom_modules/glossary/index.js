ab375468dd242143ba33c2ad0d2e3c4ccdc9c4a0
Carl Avidano <cavidano@gmail.com>
2021-08-05 10:09:50 -0400

    before John

--- src/custom_modules/glossary/index.js
+++ src/custom_modules/glossary/index.js
@@ -14,15 +14,94 @@ export default class Glossary {
 
         if(glossaryTermsButton) {
 
-            glossaryTermsButton.addEventListener('click', () => {
+            glossaryTermsButton.addEventListener('click', (event) => {
 
-                termList.forEach((term) => {
+                event.preventDefault();
+
+                glossaryTermsButton.classList.toggle('active');
+
+                termList.forEach((term, index) => {
 
                     term.classList.toggle('highlighted');
+
+                    term.setAttribute('tabindex', index + 1);
+
+                    initTooltip(term);
 
                 });
 
             });
+
+            function initTooltip(term) {
+
+                let hoverFocusDelay;
+
+                const hoverFocusIn = (event) => {
+
+                    hoverFocusDelay = setTimeout(() => { 
+
+                        termList.forEach((term) => {
+                            term.classList.remove("tooltip-show");
+                        });
+
+                        event.target.classList.add("tooltip-show");
+                    }, 300);
+                };
+
+                const hoverFocusOut = (event) => {
+                    clearTimeout(hoverFocusDelay);
+                    event.target.classList.remove("tooltip-show");
+                }
+
+                const tooltipText = term.getAttribute("data-def");
+
+                const tooltipHTML = `
+                <span class="term__tooltip">
+                    <span class="tooltip-header">Glossary Term</span>
+                    ${tooltipText}
+                </span>`;
+
+                if(tooltipText) {
+
+                    term.insertAdjacentHTML('beforeend', tooltipHTML);
+                    
+                    const buttonTooltip = term.querySelector(".term__tooltip");
+
+                    const tooltipPosition = () => {
+
+                        const buttonTooltipWidth = buttonTooltip.offsetWidth / 2;
+                        const buttonPositionLeft = term.offsetLeft ;
+                        const buttonPositionRight = window.innerWidth - (term.offsetLeft + term.offsetWidth);
+
+                        if (buttonTooltipWidth > buttonPositionLeft) {
+                            buttonTooltip.classList.add("left");
+                        }
+
+                        if (buttonTooltipWidth > buttonPositionRight) {
+                            buttonTooltip.classList.add("right");
+                        }
+
+                    };
+
+                    if (term.classList.contains('highlighted')){
+
+                        tooltipPosition();
+
+                        window.addEventListener("resize", tooltipPosition);
+
+                        term.addEventListener("mouseenter", hoverFocusIn);
+                        term.addEventListener("focusin", hoverFocusIn);
+
+                        term.addEventListener("mouseleave", hoverFocusOut);
+                        term.addEventListener("focusout", hoverFocusOut);
+                    } else {
+                        term.removeEventListener("mouseenter");
+                        term.removeEventListener("focusin");
+                    }
+
+                }
+            }
+
 
         }
 
