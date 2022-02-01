86bf9426145555d81312555a97a92b2df39f10a4
Carl Avidano <cavidano@gmail.com>
2022-02-01 09:23:51 -0500

    updating toggle

--- src/custom_modules/reading_toggles/index.js
+++ src/custom_modules/reading_toggles/index.js
@@ -8,11 +8,98 @@ export default class ReadingToggles {
 
     constructor() {
         
+        const ngssToggleSwitch = document.getElementById('ngss-toggle-switch');
+        const ngssTextList = document.querySelectorAll('.ngss');
+
+        const termsToggleSwitch = document.getElementById('terms-toggle-switch');
+
+        const turnOnNGSS = () => {
+
+            ngssTextList.forEach((mark, index) => {
+
+                mark.classList.add('highlighted');
+
+                mark.setAttribute('tabindex', index + 1);
+
+                mark.addEventListener('click', () => {
+
+                    const ngssCat = mark.getAttribute('data-ngss-cat');
+                    const ngssText = mark.getAttribute('data-ngss');
+
+                    let oldNGSSDesc = ngssDescBlock.querySelectorAll('.ngss-desc-block__text');
+
+                    oldNGSSDesc.forEach((desc) => {
+                        desc.remove();
+                    });
+
+                    const ngssDescHTML = (`
+                        <article class="ngss-desc-block__text box-shadow-1" data-ngss-cat="${ngssCat}" aria-polite="live">
+                            <p class="font-size-md text-transform-uppercase margin-bottom-1">
+                                <strong>
+                                    <em>${ngssCat}</em>
+                                </strong>
+                            </p>
+                            <p>
+                                ${ngssText ? ngssText : 'That is not good.'}
+                            </p>
+                        </article>
+                    `);
+
+                    ngssDescBlock.insertAdjacentHTML('beforeend', ngssDescHTML);
+
+                    let ngssDescBlockText = ngssDescBlock.querySelector('.ngss-desc-block__text');
+
+                    setTimeout(() => {ngssDescBlockText.classList.add('shown')}, 20);
+
+                });
+
+            });
+        }
+
+        const turnOffNGSS = () => {
+
+            ngssToggleSwitch.checked = false;
+
+            ngssTextList.forEach((mark) => {
+
+                mark.classList.remove('highlighted');
+
+                mark.setAttribute('tabindex', -1);
+
+                let oldNGSSDesc = ngssDescBlock.querySelectorAll('.ngss-desc-block__text');
+
+                oldNGSSDesc.forEach((desc) => {
+                    desc.remove();
+                });
+
+            });
+        }
+
+        //////////////////////////////////////////////
+        // Terms
+        //////////////////////////////////////////////
+
+        if(termsToggleSwitch){
+
+            termsToggleSwitch.addEventListener('change', (e) => {
+
+            const highlightTerms = e.target.checked;
+
+            if(highlightTerms === true){
+
+                if(ngssToggleSwitch.checked === true) {
+                    turnOffNGSS();
+                }
+
+            }
+
+
+            });
+        }
+        
         //////////////////////////////////////////////
         // NGSS
         //////////////////////////////////////////////
-
-        const ngssToggleSwitch = document.getElementById('ngss-toggle-switch');
 
         const ngssDescBlock = document.querySelector('.ngss-desc-block');
 
@@ -20,7 +107,6 @@ export default class ReadingToggles {
 
         if(ngssToggleSwitch) {
 
-            const ngssTextList = document.querySelectorAll('.ngss');
 
             ngssToggleSwitch.addEventListener('change', (e) => {
 
@@ -28,66 +114,22 @@ export default class ReadingToggles {
 
                 if(highlightNGSS === true) {
 
-                    ngssTextList.forEach((mark, index) => {
+                    // Turn Terms Toggle Off
 
-                        mark.classList.add('highlighted');
+                    if(termsToggleSwitch) {
+                        termsToggleSwitch.checked = false;
+                    }
 
-                        mark.setAttribute('tabindex', index + 1);
-
-                        mark.addEventListener('click', () => {
-
-                            const ngssCat = mark.getAttribute('data-ngss-cat');
-                            const ngssText = mark.getAttribute('data-ngss');
-
-                            let oldNGSSDesc = ngssDescBlock.querySelectorAll('.ngss-desc-block__text');
-
-                            oldNGSSDesc.forEach((desc) => {
-                                desc.remove();
-                            });
-
-                            const ngssDescHTML = (`
-                                <article class="ngss-desc-block__text box-shadow-1" data-ngss-cat="${ngssCat}" aria-polite="live">
-                                    <p class="font-size-md text-transform-uppercase margin-bottom-1">
-                                        <strong>
-                                            <em>${ngssCat}</em>
-                                        </strong>
-                                    </p>
-                                    <p>
-                                        ${ngssText ? ngssText : 'That is not good.'}
-                                    </p>
-                                </article>
-                            `);
-
-                            ngssDescBlock.insertAdjacentHTML('beforeend', ngssDescHTML);
-
-                            let ngssDescBlockText = ngssDescBlock.querySelector('.ngss-desc-block__text');
-
-                            setTimeout(() => {ngssDescBlockText.classList.add('shown')}, 20);
-
-                        })
-                    });
+                    turnOnNGSS();
 
                 } else {
 
-                    ngssTextList.forEach((mark) => {
-
-                        mark.classList.remove('highlighted');
-
-                        mark.setAttribute('tabindex', -1);
-
-                        let oldNGSSDesc = ngssDescBlock.querySelectorAll('.ngss-desc-block__text');
-
-                        oldNGSSDesc.forEach((desc) => {
-                            desc.remove();
-                        });
-
-
-                    });
+                    turnOffNGSS();
 
                 }
-        
-            });
 
+            }); //
+        
         }       
 
     }
