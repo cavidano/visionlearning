0006636fe75c3163537835fb59be5e62f4f106b3
Carl Avidano <cavidano@gmail.com>
2022-08-16 05:26:21 -0400

    before cleaning up templates

--- webpack.dev.js
+++ webpack.dev.js
@@ -17,7 +17,7 @@ const devPages = {
 
 const devDir = './_tearsheet';
 
-const devPage = `${devPages.Accordion}.html`;
+const devPage = `${devPages.DisciplineGrid}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
