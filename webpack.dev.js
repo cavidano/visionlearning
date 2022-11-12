6439210fddbb2f491e0e664f42759ca4ed6d02ab
Carl Avidano <cavidano@gmail.com>
2022-11-12 02:59:28 -0500

    updating fonts

--- webpack.dev.js
+++ webpack.dev.js
@@ -22,7 +22,7 @@ const devPages = {
 
 const devDir = './_tearsheet';
 
-const devPage = `${devPages.Color}.html`;
+const devPage = `${devPages.ReadingToggles}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
