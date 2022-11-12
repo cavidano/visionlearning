16ee60f2ebd786979569f6443be65d90a113f399
Carl Avidano <cavidano@gmail.com>
2022-11-12 05:07:57 -0500

    fixed form bug

--- webpack.dev.js
+++ webpack.dev.js
@@ -22,7 +22,7 @@ const devPages = {
 
 const devDir = './_tearsheet';
 
-const devPage = `${devPages.ReadingToggles}.html`;
+const devPage = `${devPages.Quiz}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
