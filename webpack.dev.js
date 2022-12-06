714f6355b5dcc4e7f536a5c94aa0b3efd94220b9
Carl Avidano <cavidano@gmail.com>
2022-12-06 05:17:24 -0500

    before cleaning up module sidebar

--- webpack.dev.js
+++ webpack.dev.js
@@ -25,7 +25,7 @@ const devPages = {
 
 const devDir = './_tearsheet';
 
-const devPage = `${devPages.Grid}.html`;
+const devPage = `${devPages.Module}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
