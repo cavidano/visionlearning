f6e9335685bf5fb7c84b7217ec071f9d6fb39e5f
Carl Avidano <cavidano@gmail.com>
2022-12-29 06:31:57 -0500

    adding new course form

--- webpack.dev.js
+++ webpack.dev.js
@@ -26,7 +26,7 @@ const devPages = {
 
 const devDir = './_tearsheet';
 
-const devPage = `${devPages.Accordion}.html`;
+const devPage = `${devPages.List}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
