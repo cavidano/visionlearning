7adb9e5a79e2e8d18a91194840d7a6e723632fa7
Carl Avidano <cavidano@gmail.com>
2023-01-05 06:38:02 -0500

    cleaning up lists

--- webpack.dev.js
+++ webpack.dev.js
@@ -26,7 +26,7 @@ const devPages = {
 
 const devDir = './_tearsheet';
 
-const devPage = `${devPages.HeaderFooter}.html`;
+const devPage = `${devPages.List}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
