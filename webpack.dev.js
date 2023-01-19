22fde8edf81009d5f32d2311912a05be2a5abf5e
Carl Avidano <cavidano@gmail.com>
2023-01-19 06:26:57 -0500

    adding figure examples

--- webpack.dev.js
+++ webpack.dev.js
@@ -26,7 +26,7 @@ const devPages = {
 
 const devDir = './_tearsheet';
 
-const devPage = `${devPages.DisciplineGrid}.html`;
+const devPage = `${devPages.Module}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
