1f5fd16e5b802209b4f903b5eab98e2c31ab274b
Carl Avidano <cavidano@gmail.com>
2023-03-11 12:57:41 -0500

    added terser

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,63 +5,68 @@ const path = require('path');
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
-    mode: 'production',
-    target: 'browserslist',
-    output: {
-        filename: 'js/[name].js',
-        path: path.resolve(__dirname, 'dist'),
-        iife: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            [
-                                '@babel/preset-env'
-                            ]
-                        ],
-                        plugins: [
-                            '@babel/plugin-transform-shorthand-properties',
-                            '@babel/plugin-proposal-object-rest-spread'
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    [
-                                        'autoprefixer'
-                                    ],
-                                ],
-                            },
-                        },
-                    },
-                    'sass-loader',
-                ],
-            },
-        ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].css',
-        }),
-        new CssMinimizerPlugin()
-    ]
+	mode: 'production',
+	target: 'browserslist',
+	output: {
+		filename: 'js/[name].js',
+		path: path.resolve(__dirname, 'dist'),
+		iife: true,
+	},
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({
+				terserOptions: {
+					compress: {
+						drop_console: true,
+					},
+				},
+			}),
+		],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [['@babel/preset-env']],
+						plugins: [
+							'@babel/plugin-transform-shorthand-properties',
+							'@babel/plugin-proposal-object-rest-spread',
+						],
+					},
+				},
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: {
+								plugins: [['autoprefixer']],
+							},
+						},
+					},
+					'sass-loader',
+				],
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].css',
+		}),
+		new CssMinimizerPlugin(),
+	],
 });
