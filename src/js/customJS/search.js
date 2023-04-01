209e1c8cd37e4cf29be78e880f5b39e39c7efda3
Carl Avidano <cavidano@gmail.com>
2023-04-01 11:07:04 -0400

    adding search pages

--- src/js/customJS/search.js
+++ src/js/customJS/search.js
@@ -1,39 +1,49 @@
-export default class Search {
+import { highlightMatchedWord } from '../utilities';
 
-    #searchInput = document.querySelector('#search-input');
-    #clearButton = document.querySelector('#search-clear');
+export default class Search {
+	#searchInput = document.querySelector('#search-input');
+	#clearButton = document.querySelector('#search-clear');
+	
+    #searchResults = document.querySelector('#search-results');
 
 	#handleSearchInput = (e) => {
 		const searchValue = e.target.value;
 		this.#clearButton.style.display = searchValue ? 'flex' : 'none';
-	}
+	};
 
 	#handleSearchChange = () => {
-        const isHidden = this.#clearButton.getAttribute('aria-hidden') === 'true';
-        this.#clearButton.setAttribute('aria-hidden', !isHidden);
-	}
+		const isHidden = this.#clearButton.getAttribute('aria-hidden') === 'true';
+		this.#clearButton.setAttribute('aria-hidden', !isHidden);
+	};
 
 	#handleClearButtonClick = (e) => {
-        e.preventDefault();
+		e.preventDefault();
 		this.#searchInput.value = '';
-        this.#searchInput.focus();
+		this.#searchInput.focus();
 
 		this.#clearButton.style.display = 'none';
-	}
+	};
 
-    init() {
-
-        if(this.#searchInput) {
-    
-            if(this.#searchInput.value) {        
-                this.#clearButton.style.display = 'flex';
-                this.#clearButton.setAttribute('aria-hidden', false);
-            }
-
-            this.#searchInput.addEventListener('input', this.#handleSearchInput);
-            this.#searchInput.addEventListener('change', this.#handleSearchChange);
-
-            this.#clearButton.addEventListener('click', this.#handleClearButtonClick);
-        }
+    #handleFormSubmit = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            highlightMatchedWord(this.#searchResults, this.#searchInput.value);
+        };
     }
+
+	init() {
+		if (this.#searchInput) {
+
+			if (this.#searchInput.value) {
+				this.#clearButton.style.display = 'flex';
+				this.#clearButton.setAttribute('aria-hidden', false);
+			}
+
+			this.#searchInput.addEventListener('input', this.#handleSearchInput);
+			this.#searchInput.addEventListener('change', this.#handleSearchChange);
+
+			this.#clearButton.addEventListener('click', this.#handleClearButtonClick);
+            this.#searchInput.addEventListener('keydown', this.#handleFormSubmit);
+		}
+	}
 }
--- src/js/utilities/index.js
+++ src/js/utilities/index.js
@@ -80,3 +80,84 @@ export const matchMedia = ((size) => {
   return window.matchMedia(`(min-width: ${size}px)`);
 
 });
+
+//////////////////////////////////////////////
+// D. Highlight Matched Text
+//////////////////////////////////////////////
+
+const clearHighlights = (target, className) => {
+
+  const highlightedElements = target.querySelectorAll(`span.${className}`);
+
+  highlightedElements.forEach((element) => {
+    const parent = element.parentNode;
+    while (element.firstChild) {
+      parent.insertBefore(element.firstChild, element);
+    }
+    parent.removeChild(element);
+  });
+
+};
+
+export const highlightMatchedWord = (target, word) => {
+
+// Clear existing highlights
+  clearHighlights(target, 'coolest');
+
+    // Regular expression to match all occurrences of the word, 
+    // including partial matches and regardless of case
+    const regex = new RegExp(word, "gi");
+
+    // Get all the text nodes on the page
+    const nodes = document.createTreeWalker(target, NodeFilter.SHOW_TEXT);
+
+    const highlightTextNode = (textNode, matches) => {
+        const text = textNode.nodeValue;
+        const fragment = document.createDocumentFragment();
+        let lastIndex = 0;
+
+        matches.forEach((match) => {
+          const matchedText = match[0];
+          const matchedIndex = match.index;
+
+          // Append the text before the match
+          fragment.appendChild(document.createTextNode(text.slice(lastIndex, matchedIndex)));
+
+          // Append the highlighted match with its original case
+          const span = document.createElement("span");
+          span.classList.add('coolest');
+          span.style.backgroundColor = "yellow";
+          span.appendChild(document.createTextNode(matchedText));
+          fragment.appendChild(span);
+
+          lastIndex = matchedIndex + matchedText.length;
+        });
+
+        // Append the remaining text after the last match
+        fragment.appendChild(document.createTextNode(text.slice(lastIndex)));
+
+        // Replace the original text node with the new nodes
+        const parent = textNode.parentNode;
+        parent.replaceChild(fragment, textNode);
+    };
+
+    const textNodes = [];
+
+    // Loop through each text node and store them in an array
+    while (nodes.nextNode()) {
+        const node = nodes.currentNode;
+        const text = node.nodeValue;
+        regex.lastIndex = 0;
+
+        if (text && regex.test(text)) {
+        textNodes.push(node);
+        }
+    }
+
+    // Loop through each stored text node and highlight the matches
+    textNodes.forEach((node) => {
+        regex.lastIndex = 0;
+        const matches = Array.from(node.nodeValue.matchAll(regex));
+        highlightTextNode(node, matches);
+    });
+};
