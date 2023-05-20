803d8ee4ab79bbe26464b6292a152e3e9afaf19b
Carl Avidano <cavidano@gmail.com>
2023-05-20 09:34:42 -0400

    cleaning up imports

--- src/index.js
+++ src/index.js
@@ -14,9 +14,8 @@ In this file:
 import './scss/styles.scss';
 
 //////////////////////////////////////////////
-// B. JS
+// B. JS Modules
 //////////////////////////////////////////////
-
 
 import Alert from './js/alert';
 const alert = new Alert();
@@ -34,6 +33,10 @@ import Form from './js/form';
 const form = new Form();
 form.init();
 
+import Lightbox from './js/lightbox';
+const lightbox = new Lightbox();
+lightbox.init();
+
 import Navigation from './js/navigation';
 const navigation = new Navigation();
 navigation.init();
@@ -47,41 +50,31 @@ const tab = new Tab();
 tab.init();
 
 //////////////////////////////////////////////
-// Custom Modules
+// C. Custom JS Modules
 //////////////////////////////////////////////
 
 import Accordion from './js/customJS/accordion';
 const accordion = new Accordion();
 accordion.init();
 
-import Layout from './js/customJS/layout'; 
+import { AnchorNav } from './js/customJS/table-of-contents';
+const anchorNav = new AnchorNav();
+anchorNav.init();
 
+import { CompCheck } from './js/customJS/quiz'; 
+const compCheck = new CompCheck();
+compCheck.init();
+
+import Layout from './js/customJS/layout'; 
 const layout = new Layout();
 layout.init();
 
 import Modal from './js/customJS/modal';
-
 const modal = new Modal();
 modal.init();
 
-
-import { CompCheck } from './js/customJS/quiz'; 
-
-const compCheck = new CompCheck();
-compCheck.init();
-
 import ReadingToggles from './js/customJS/reading-toggles';
 new ReadingToggles();
-
-import Lightbox from './js/lightbox'
-const lightbox = new Lightbox();
-lightbox.init();
-
-import { AnchorNav } from './js/customJS/table-of-contents';
-
-// Create a new instance of the TableOfContents class
-const anchorNav = new AnchorNav();
-anchorNav.init();
 
 import Search from './js/customJS/search';
 const search = new Search();
