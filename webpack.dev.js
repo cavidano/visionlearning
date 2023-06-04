bbf841ecb8d39187fa0cddab33431c65a2b0aeee
Carl Avidano <cavidano@gmail.com>
2023-06-04 10:43:45 -0400

    including comments for Russ

--- webpack.dev.js
+++ webpack.dev.js
@@ -29,7 +29,7 @@ const devPages = {
 
 const devDir = './_tearsheet';
 
-const devPage = `${devPages.HeaderFooter}.html`;
+const devPage = `${devPages.DisciplineGrid}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
