78b2ab4eac154e36296d9c10c3d8cf1135aac5cb
Carl Avidano <cavidano@gmail.com>
2023-06-19 14:03:39 -0400

    adding audio player

--- src/js/customJS/audio-player.js
+++ src/js/customJS/audio-player.js
@@ -1,59 +1,13 @@
 export default class AudioPlayer {
-    // Define the properties we're going to use.
-    audio;
-    playPauseButton;
-    progressBar;
-    progress;
-    volumeSlider;
-    timestamp;
-    isPlaying = false;
+    #audio;
+    #playPauseButton;
+    #progressBar;
+    #progress;
+    #volumeSlider;
+    #timestamp;
+    #isPlaying;
 
-    // The initialization method
-    init() {
-        this.audio = document.querySelector('.audio-player audio');
-        this.playPauseButton = document.querySelector('.audio-player__controls .button--icon-only');
-        this.progressBar = document.querySelector('.audio-player__progress');
-        this.progress = document.querySelector('.audio-player__progress div');
-        this.volumeSlider = document.querySelector('.audio-player__volume input[type="range"]');
-        this.timestamp = document.querySelector('.audio-player__timestamp');
-
-        this.setupEventListeners();
-    }
-
-    // The function to setup our event listeners
-    setupEventListeners() {
-        this.playPauseButton.addEventListener('click', this.togglePlayPause.bind(this));
-        this.audio.addEventListener('timeupdate', this.updateProgress.bind(this));
-        this.volumeSlider.addEventListener('input', this.updateVolume.bind(this));
-        this.progressBar.addEventListener('click', this.seek.bind(this));
-    }
-
-    togglePlayPause() {
-        this.isPlaying ? this.audio.pause() : this.audio.play();
-        this.isPlaying = !this.isPlaying;
-        this.toggleClasses(this.playPauseButton.querySelector('span.icon'), 'icon-play', 'icon-pause');
-    }
-
-    updateProgress() {
-        const progressPercentage = (this.audio.currentTime / this.audio.duration) * 100;
-        this.progress.style.width = `${progressPercentage}%`;
-
-        const minutes = Math.floor(this.audio.currentTime / 60);
-        const seconds = Math.floor(this.audio.currentTime - minutes * 60);
-        this.timestamp.innerText = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-    }
-
-    updateVolume() {
-        this.audio.volume = this.volumeSlider.value;
-    }
-
-    seek(e) {
-        const clickPositionInBar = e.clientX - this.progressBar.getBoundingClientRect().left;
-        const progressPercentage = (clickPositionInBar / this.progressBar.offsetWidth) * 100;
-        this.audio.currentTime = (progressPercentage / 100) * this.audio.duration;
-    }
-
-    toggleClasses(element, class1 = null, class2 = null) {
+    #toggleClasses(element, class1 = null, class2 = null) {
         if (element.classList.contains(class1)) {
             element.classList.remove(class1);
             element.classList.add(class2);
@@ -61,5 +15,62 @@ export default class AudioPlayer {
             element.classList.remove(class2);
             element.classList.add(class1);
         }
+    }
+
+    #togglePlayPause() {
+        this.#isPlaying ? this.#audio.pause() : this.#audio.play();
+        this.#isPlaying = !this.#isPlaying;
+        this.#playPauseButton.setAttribute('aria-label', this.#isPlaying ? 'Pause Audio' : 'Play Audio');
+        this.#toggleClasses(this.#playPauseButton.querySelector('span.icon'), 'icon-play', 'icon-pause');
+    }
+
+    #updateProgress() {
+        const progressPercentage = (this.#audio.currentTime / this.#audio.duration) * 100;
+        this.#progress.style.width = `${progressPercentage}%`;
+        
+        const minutes = Math.floor(this.#audio.currentTime / 60);
+        const seconds = Math.floor(this.#audio.currentTime - minutes * 60);
+
+        this.#timestamp.innerText = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    }
+
+    #setVolume() {
+        this.#audio.volume = this.#volumeSlider.value;
+    }
+
+    #setTime(e) {
+        const clickPositionInBar = e.clientX - this.#progressBar.getBoundingClientRect().left;
+        const progressPercentage = (clickPositionInBar / this.#progressBar.offsetWidth) * 100;
+        this.#audio.currentTime = (progressPercentage / 100) * this.#audio.duration;
+    }
+
+    #handleKeyDown(e) {
+        const step = 5; // Number of seconds to step
+        if(e.key === 'ArrowRight') {
+            this.#audio.currentTime = Math.min(this.#audio.currentTime + step, this.#audio.duration);
+        } else if(e.key === 'ArrowLeft') {
+            this.#audio.currentTime = Math.max(this.#audio.currentTime - step, 0);
+        }
+        this.#updateProgress();
+    }
+
+    init() {
+        this.#audio = document.querySelector('#audio');
+        this.#playPauseButton = document.querySelector('#play-pause-button');
+        this.#progressBar = document.querySelector('#progress-bar');
+        this.#progress = document.querySelector('#progress');
+        this.#volumeSlider = document.querySelector('#volume-slider');
+        this.#timestamp = document.querySelector('#timestamp');
+
+        this.#isPlaying = false;
+
+        this.#playPauseButton.addEventListener('click', () => this.#togglePlayPause());
+        this.#audio.addEventListener('timeupdate', () => this.#updateProgress());
+        this.#volumeSlider.addEventListener('input', () => this.#setVolume());
+        this.#progressBar.addEventListener('click', (e) => this.#setTime(e));
+        this.#progressBar.addEventListener('keydown', (e) => this.#handleKeyDown(e));
+
+        // Make the progress bar focusable
+        this.#progressBar.setAttribute('tabindex', '0');
     }
 }
