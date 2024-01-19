c0507d99e0d1120c71a7cb7adb81d6143b9d2036
Carl Avidano <cavidano@gmail.com>
2024-01-19 07:07:16 -0500

    fixed empty p tag in blockquote

--- webpack.dev.js
+++ webpack.dev.js
@@ -31,7 +31,7 @@ const devPages = {
 
 const devDir = './html';
 
-const devPage = `${devPages.List}.html`;
+const devPage = `${devPages.Typography}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
