cf4ce521ad99b8acbd59e5d14983d78d85b63b10
Carl Avidano <cavidano@gmail.com>
2024-06-22 17:39:57 -0400

    before adding event del

--- src/index.js
+++ src/index.js
@@ -35,61 +35,58 @@ import BackToTop from './js/customJS/back-to-top';
 // C. Class Instantiation
 //////////////////////////////////////////////
 
-document.addEventListener('DOMContentLoaded', () => {
+const alertDismissable = new AlertDismissable();
+alertDismissable.init();
 
-    const alertDismissable = new AlertDismissable();
-    alertDismissable.init();
+const button = new Button();
+button.init();
 
-    const button = new Button();
-    button.init();
+const collapse = new Collapse();
+collapse.init();
 
-    const collapse = new Collapse();
-    collapse.init();
+const formInput = new FormInput();
+formInput.init();
 
-    const formInput = new FormInput();
-    formInput.init();
+const formSubmission = new FormSubmission();
+formSubmission.init();
 
-    const formSubmission = new FormSubmission();
-    formSubmission.init();
+const formFileUpload = new FormFileUpload();
+formFileUpload.init();
 
-    const formFileUpload = new FormFileUpload();
-    formFileUpload.init();
+const lightbox = new Lightbox();
+lightbox.init();
 
-    const lightbox = new Lightbox();
-    lightbox.init();
+const navigation = new Navigation();
+navigation.init();
 
-    const navigation = new Navigation();
-    navigation.init();
+const table = new Table();
+table.init();
 
-    const table = new Table();
-    table.init();
+const tab = new Tab();
+tab.init();
 
-    const tab = new Tab();
-    tab.init();
+const accordion = new Accordion();
+accordion.init();
 
-    const accordion = new Accordion();
-    accordion.init();
+// Custom JS Modules
 
-    // Custom JS Modules
+const audioPlayer = new AudioPlayer();
+audioPlayer.init();
 
-    const audioPlayer = new AudioPlayer();
-    audioPlayer.init();
+const anchorNav = new AnchorNav();
+anchorNav.init();
 
-    const anchorNav = new AnchorNav();
-    anchorNav.init();
+const compCheck = new CompCheck();
+compCheck.init();
 
-    const compCheck = new CompCheck();
-    compCheck.init();
+const layout = new Layout();
+layout.init();
 
-    const layout = new Layout();
-    layout.init();
+const modal = new Modal();
+modal.init();
 
-    const modal = new Modal();
-    modal.init();
+const readingToggles = new ReadingToggles();
+readingToggles.init();
 
-    const readingToggles = new ReadingToggles();
-    readingToggles.init();
-
-    const backToTop = new BackToTop();
-    backToTop.init();
-});
+const backToTop = new BackToTop();
+backToTop.init();
