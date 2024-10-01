1aabc4803b3a8a1eba39aa0f92a8e7278a894717
Carl Avidano <cavidano@gmail.com>
2024-10-01 07:23:35 -0400

    cleaning up acc

--- webpack.dev.js
+++ webpack.dev.js
@@ -31,7 +31,7 @@ const devPages = {
 
 const devDir = './html';
 
-const devPage = `${devPages.HeaderFooter}.html`;
+const devPage = `${devPages.Accordion}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
