6c15fe5ba234ce3cff41866b545032b05e794560
Carl Avidano <cavidano@gmail.com>
2024-10-07 15:55:11 -0400

    Updating tabs

--- webpack.dev.js
+++ webpack.dev.js
@@ -31,7 +31,7 @@ const devPages = {
 }
 
 const devDir = './html';
-const devPage = `${devPages.Module}.html`;
+const devPage = `${devPages.Navigation}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
